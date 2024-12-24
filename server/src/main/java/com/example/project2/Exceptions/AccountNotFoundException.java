31ecbb4fc7e559961d4b70545b6dfbc465463df0
Junyi-Pan <junyipan8460@gmail.com>
2024-12-24 14:12:40 -0500

    Password hashing and code documentation

--- server/src/main/java/com/example/project2/Exceptions/AccountNotFoundException.java
+++ server/src/main/java/com/example/project2/Exceptions/AccountNotFoundException.java
@@ -1,5 +1,8 @@
 package com.example.project2.Exceptions;
 
+/*
+ * Exception to throw if account is not in db
+ */
 public class AccountNotFoundException extends Exception{
 
 }
--- server/src/main/java/com/example/project2/Exceptions/PasswordIncorrectException.java
+++ server/src/main/java/com/example/project2/Exceptions/PasswordIncorrectException.java
@@ -1,5 +1,8 @@
 package com.example.project2.Exceptions;
 
+/*
+ * Exception to throw for invalid login
+ */
 public class PasswordIncorrectException extends Exception{
 
 }
