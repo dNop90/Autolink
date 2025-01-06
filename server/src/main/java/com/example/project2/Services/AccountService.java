a706f436e63bf6709ac6aa03a59cff817c88d37a
D <zwolfie.90@gmail.com>
2025-01-06 14:28:04 -0500

    Moved JWT secret key to application.properties

--- server/src/main/java/com/example/project2/Services/AccountService.java
+++ server/src/main/java/com/example/project2/Services/AccountService.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
