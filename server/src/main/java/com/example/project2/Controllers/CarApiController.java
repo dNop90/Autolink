df2585f60b67a02d1e1ceae52518e367a92e9cc3
Lian Hong <biakhong@gmail.com>
2025-01-09 13:27:17 -0500

    Auth Login CroosOrigin fixed

--- server/src/main/java/com/example/project2/Controllers/CarApiController.java
+++ server/src/main/java/com/example/project2/Controllers/CarApiController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 @RequestMapping("/api")
 public class CarApiController {
 
